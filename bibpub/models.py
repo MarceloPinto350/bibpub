dff2d57c668ed8c96e2939e9e460183c1753bd6d
Alikson Oliveira <alikson.oliveira@ifrn.edu.br>
2023-11-14 00:30:23 -0300

    verbose name do modelo categoria

--- bibpub/models.py
+++ bibpub/models.py
@@ -1,12 +1,14 @@
 from django.db import models
 
 class Categoria(models.Model):
-    descricao = models.TextField(blank=True)
+    descricao = models.TextField('Descrição', unique=True)
 
     def __str__(self):
         return self.descricao
 
     class Meta:
+        verbose_name = 'Categoria'
+        verbose_name_plural = 'Categorias'
         permissions = [
             ("can_view_categoria", "Can view categorias"),
             ("can_change_categoria", "Can change categorias"),
