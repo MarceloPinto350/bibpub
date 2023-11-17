6dd45f3379ea7e72ee764a2c945a479b58190c11
Marcelo Pinto <pintomarc@gmail.com>
2023-11-17 10:38:28 -0300

    Ajustes nas definições dos modelos.

--- bibpub/models.py
+++ bibpub/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 
+# outros imports usados
+from datetime import datetime
+
 class Categoria(models.Model):
     descricao = models.TextField('Descrição', unique=True)
 
@@ -39,7 +42,7 @@ class Pessoa (models.Model):
     OrigemCadastro = models.TextChoices("Origem cadastro" ,"Internet Aplicação")
     SituacaoCadastro = models.TextChoices("Situação cadastro","Ativo Pendente Suspenso Cancelado")
     #id = models.BigAutoField(primary_key=True)
-    nome = models.CharField("Nome", max_length=200, null=False)
+    nome = models.CharField("Nome", max_length=200, null=False,db_index=True)
     nascimento = models.DateField("Data nascimento")
     cpf = models.CharField("CPF", max_length=14, null=False, unique=True)
     #criar validador para CPF
@@ -65,7 +68,7 @@ class Pessoa (models.Model):
     
     # retornar o valor padrão para a classe
     def __str__(self):
-        return f"{self.cpf} - {self.nome}"
+        return self.nome
 
     # define o nome padrão da tabela a ser criada no BD
     class Meta:
@@ -73,12 +76,11 @@ class Pessoa (models.Model):
         verbose_name = 'Pessoa'
         verbose_name_plural = 'Pessoas'
         permissions = [
-            ("can_view_pessoa", "Pode ver pessoas"),
-            ("can_change_pessoa", "Pode alterar pessoas"),
-            ("can_add_pessoa", "Pode adicionar pessoas"),
-            ("can_delete_pessoa", "Pode apagar pessoas"),
+            ("can_view_pessoa", "Can view pessoas"),
+            ("can_change_pessoa", "Can change pessoas"),
+            ("can_add_pessoa", "Can add pessoas"),
+            ("can_delete_pessoa", "Can delete pessoas"),
         ]
-
 
 
 # Definição do modelo de Autor
@@ -87,11 +89,20 @@ class Autor(models.Model):
     nascimento = models.DateField("Data de nascimento", null=True)
     biografia = models.TextField("Biografia do autor", max_length=1000, null=False)
  
+    def __str__(self):
+        return self.nome 
+
     # define as configuraçẽos da classe Meta (dados de BD)
     class Meta:
         db_table = "tb_autor"
         verbose_name = "Autor"
         verbose_name_plural = "Autores"
+        permissions = [
+            ("can_view_autor", "Can view autores"),
+            ("can_change_autor", "Can change autores"),
+            ("can_add_autor", "Can add autores"),
+            ("can_delete_autor", "Can delete autores"),
+        ]
 
 
 # Definição do modelo de Autor
@@ -101,33 +112,51 @@ class Editora(models.Model):
     # criar validação para os e-mails
     pais = models.CharField("Pas da editora", max_length=100, null=False)
     # validar conformme tabela de países a ser criada
- 
+
+    def __str__(self):
+        return self.nome 
+    
     # define as configurações da classe Meta (dados de BD)
     class Meta:
         db_table = "tb_editora"
         verbose_name = "Editora"
         verbose_name_plural = "Editoras"
-
+        permissions = [
+            ("can_view_editora", "Can view editoras"),
+            ("can_change_editora", "Can change editoras"),
+            ("can_add_editora", "Can add editoras"),
+            ("can_delete_editora", "Can delete editoras"),
+        ]
 
 # Definição do modelo de Obra
 class Obra(models.Model):
     TipoObra = models.TextChoices("Tipo de obra" ,"Livro Periódico Jornal Outro")
-    titulo = models.CharField("Título da obra", max_length=200, null=False, unique=True)
+    titulo = models.CharField("Título da obra", max_length=200, null=False, unique=True,db_index=True)
     anopublicacao = models.PositiveSmallIntegerField("Ano de publicação da obra", null=False)
     # criar validação para o maior valor ser o ano corrente e o menor 100
     edicao  = models.PositiveSmallIntegerField("Número da edição da obra", default=1, null=False)
     isbn  = models.CharField("Número do ISBN da obra", null=True)   # se for livro
     issn  = models.CharField("Número do ISSN da obra", null=True)   # se for periódico
     tipo  = models.CharField("Tipo de obra", max_length=10, null=False, choices=TipoObra.choices)
-    categoria = models.ForeignKey (Categoria,on_delete=models.CASCADE,)
-    autor =  models.ForeignKey (Autor,on_delete=models.CASCADE,)
-    editora = models.ForeignKey (Editora,on_delete=models.CASCADE,)
-
+    datacadastro = models.DateTimeField("Data de registro da obra",auto_now_add=True,null=False,db_index=True)
+    categoria = models.ForeignKey (Categoria,on_delete=models.CASCADE,null=False)
+    autor =  models.ForeignKey (Autor,on_delete=models.CASCADE,null=False)
+    editora = models.ForeignKey (Editora,on_delete=models.CASCADE,null=False)
+    
+    def __str__(self):
+        return self.titulo    
+        
     # define as configurações da classe Meta (dados de BD)
     class Meta:
         db_table = "tb_obra"
         verbose_name = 'Obra'
         verbose_name_plural = 'Obras'
+        permissions = [
+            ("can_view_obra", "Can view obras"),
+            ("can_change_obra", "Can change obras"),
+            ("can_add_obra", "Can add obras"),
+            ("can_delete_obra", "Can delete obras"),
+        ]
 
 
 # Definição do modelo de Emprestimo
@@ -146,20 +175,13 @@ class Emprestimo(models.Model):
         db_table = "tb_emprestimo"
         verbose_name = 'Empréstimo'
         verbose_name_plural = 'Empréstimos'
+        permissions = [
+            ("can_view_emprestimo", "Can view emprestimos"),
+            ("can_change_emprestimo", "Can change emprestimos"),
+            ("can_add_emprestimo", "Can add emprestimos"),
+            ("can_delete_emprestimo", "Can delete emprestimos"),
+        ]
 
-
-# Definição do modelo de papel
-#class Papel(models.Model):
-#    nome = models.CharField("Papel", max_length=50, null=False, unique=True)
-#    descricao = models.CharField("Descrição", max_length=200, null=False)
-#    
-#    # retornar o valor padrão para a classe
-#    def __str__(self):
-#        return f"{self.nome} - {self.descricao}"
-# 
-#    # define o nome padrão da tabela a ser criada no BD
-#    class Meta:
-#        db_table = "tb_papel"
 
 
 # Definição do modelo de papel
