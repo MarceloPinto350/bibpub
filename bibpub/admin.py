29db855f4689d32764970082a347255a506e0e42
Alikson Oliveira <alikson.oliveira@ifrn.edu.br>
2023-11-25 15:25:19 -0300

    Tela de login e ajuste necessários

--- bibpub/admin.py
+++ bibpub/admin.py
@@ -1,17 +1,25 @@
 from django.contrib import admin
 from django.contrib.admin.options import ModelAdmin
+from django.contrib.auth.models import User, Group
+from .models import Categoria, Autor, Obra
 
-from bibpub.models import Editora
-from .models import Categoria,Autor,Obra
+
+class CustomAdminSite(admin.AdminSite):
+    site_header = 'Biblioteca Pública'
+    site_title = 'Biblioteca Pública'
+    index_title = 'Bem-vindo à Biblioteca Pública'
+
+admin.site = CustomAdminSite()
 
 class CategoriaAdmin(ModelAdmin):
     list_display = ('descricao',)
     list_display_icons = True
     list_per_page = 20
-
+    
 admin.site.register(Categoria, CategoriaAdmin)
 
-
+admin.site.register(User)
+admin.site.register(Group)
 #class AutorAdmin(ModelAdmin):
 #    list_display = ('nome','nascimento','biografia')
 #    list_display_icons = True
